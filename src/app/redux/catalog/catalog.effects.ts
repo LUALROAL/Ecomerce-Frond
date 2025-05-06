405d241fee60b431895fd7a15ab6b5fdbc59a7f6
LUALROAL <alejorodryguez@gmail.com>
2025-05-05 20:32:06 -0500

    Refactor CatalogEffects to improve category loading logic and enhance error handling

--- src/app/redux/catalog/catalog.effects.ts
+++ src/app/redux/catalog/catalog.effects.ts
@@ -3,28 +3,33 @@ import { Injectable } from "@angular/core";
 import { createEffect, Actions, ofType } from "@ngrx/effects";
 import { map, mergeMap, catchError } from "rxjs/operators";
 import { of } from "rxjs";
-import { loadCategory, loadCategorySuccess, loadCategoryFailure } from "./catalog.actions";
 import { CatalogService } from "../../core/Services/catalog.service";
+import { loadCategory, loadCategoryFailure, loadCategorySuccess } from "./catalog.actions";
+
+
 
 
 
 @Injectable()
 export class CatalogEffects {
-
- loadCategory$ = createEffect(() =>
-  this.actions$.pipe(
-    ofType(loadCategory),
-    mergeMap(() => this.catalogService.getCategories()
-      .pipe(
-        map((categories) => loadCategorySuccess({ categories })),
-        catchError((error) => of(loadCategoryFailure({ error })))
-      )
-    ))
-  );
-
-
   constructor(
     private actions$: Actions,
     private catalogService: CatalogService
-  ) { }
+  ) {}
+
+  loadCategories$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadCategory),
+      mergeMap(() =>
+        this.catalogService.getCategories().pipe(
+          map((res) => {
+            return res.isSuccess === true
+              ? loadCategorySuccess({ categories: res.data ? res.data : [] })
+              : loadCategoryFailure({ error: res.message });
+          }),
+          catchError((error) => of(loadCategoryFailure({ error })))
+        )
+      )
+    )
+  );
 }
